91f40c003db729231e10f593ef261d828feb0f5d
coolMohi <coolmohhi@gmail.com>
2022-09-27 14:06:28 -0500

    twitter app 1

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 import BDBOAuth1Manager
-
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "Elov5X4vCXganL30nnrkbWMkS", consumerSecret: "vvU3FqGhoWtaonnQ22senT86C3JCnmiPVRgglZ5WwBBvKfpZe9")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
